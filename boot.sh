b09e4c5bcc76709b6bfb0612fff85be1da5fcf55
Luis Fernando Morales <lfmexi@gmail.com>
2016-02-13 02:33:29 -0600

    GW3: route for rates added, fixed error with query params

--- boot.sh
+++ boot.sh
@@ -3,5 +3,6 @@ docker run -d --name api.gateway.investments          \
  -p 3002:3000                                         \
  --link evaluation.investments:evaluation.investments \
  --link utility.investments:utility.investments       \
+ --link rates.investments:rates.investments           \
  --env-file=docker/env                                \
  lmyjo/gateway
--- config/routes/index.js
+++ config/routes/index.js
@@ -2,5 +2,6 @@ const owners = require('./owners');
 const projects = require('./projects');
 const operaciones = require('./operaciones');
 const evaluaciones = require('./evaluaciones');
+const rates = require('./rates');
 
-module.exports = [].concat(projects, owners, operaciones, evaluaciones);
+module.exports = [].concat(projects, owners, operaciones, evaluaciones, rates);
