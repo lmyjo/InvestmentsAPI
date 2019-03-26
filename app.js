3ad78e5be84bea1aee1c5c7ea4b77b655a46c9c3
Luis Morales <lfmexi@gmail.com>
2019-03-26 10:23:05 +0100

    upgrade dependencies

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
+'use strict';
+
 require('dotenv').load();
 
 const Hapi = require('hapi');
 const inert = require('inert');
 const vision = require('vision');
 const HapiSwagger = require('hapi-swagger');
-const package = require('./package');
-const routes = require('./config/routes');
 
-const server = new Hapi.Server();
+const pkg = require('./package.json');
+const routes = require('./config/routes');
 
 const swaggerOptions = {
   info: {
     title: 'Investments API',
-    version: package.version
+    version: pkg.version,
   }
 };
 
-server.connection({
+const server = Hapi.server({
   host: process.env.SERVICE_HOST,
   port: process.env.SERVICE_PORT
 });
 
 const pluginsArray = [inert, vision, {
-  register: HapiSwagger,
+  plugin: HapiSwagger,
   options: swaggerOptions
 }];
 
-server.register(pluginsArray, (err) => {
-  if(err) {
-    console.log(err);
-    return;
-  }
+server.route(routes);
 
-  server.route(routes);
+const init = async () => {
+  await server.register(pluginsArray);
 
-  server.start(function starter() {
-    console.log('Server running at: ', server.info.uri);
-  });
-});
+  await server.start();
+
+  console.log('Server running at: ', server.info.uri);
+}
+
+init()
